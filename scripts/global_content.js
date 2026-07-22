9381375c1249c79d0b57857d001bd34ab9405cdb
SeanOwenBlue <46723656+SeanOwenBlue@users.noreply.github.com>
2026-07-22 08:20:18 +0100

    Update global_content.js

--- scripts/global_content.js
+++ scripts/global_content.js
@@ -1,6 +1,11 @@
+
+
+document.getElementById('dynamic-header-content').innerHTML =`
 <img src="https://userimages-akm.imvu.com/userdata/06/11/16/71/userpics/Snap_k6kni1AH8Z563523375.png" alt="Sean Owen Blue" style="width:400px;height:400px;">
 		<h1>SEAN OWEN BLUE</h1>
 <br>
+`;
 
-document.getElementById('dynamic-announcement').innerHTML = "<h3>🎉 Summer Sale: 50% Off All Items! 🎉</h3>";
-document.getElementById('dynamic-credits').textContent = "Custom layout designed by YourName.";
+document.getElementById('dynamic-main-content').textContent = "";
+
+document.getElementById('dynamic-footer-content').innerHTML = "&#169 <script>document.write(new Date().getFullYear())</script> Sean Owen Blue";
