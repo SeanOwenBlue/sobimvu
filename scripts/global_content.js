86556c393f0b13bfe13e4bfa608c6f0772b38aac
SeanOwenBlue <46723656+SeanOwenBlue@users.noreply.github.com>
2026-07-22 08:06:35 +0100

    Create global_content.js

---
+++ scripts/global_content.js
@@ -0,0 +1,6 @@
+<img src="https://userimages-akm.imvu.com/userdata/06/11/16/71/userpics/Snap_k6kni1AH8Z563523375.png" alt="Sean Owen Blue" style="width:400px;height:400px;">
+		<h1>SEAN OWEN BLUE</h1>
+<br>
+
+document.getElementById('dynamic-announcement').innerHTML = "<h3>🎉 Summer Sale: 50% Off All Items! 🎉</h3>";
+document.getElementById('dynamic-credits').textContent = "Custom layout designed by YourName.";
